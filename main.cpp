28707308217324ae03f02da2736d31e5796d080e
Mihai Stefanoaia <mihaistefanoaia1102@gmail.com>
2021-09-21 17:49:45 +0300

    Added parallel computation | coloring still takes ages
    Couldn't parallelize the coloring, will fix that soon

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include "complexNumber.h"
 #include <iostream>
 #include <list>
+#include <omp.h>
 #define MAX_ITERATIONS 255
-#define OUT_OF_BOUNDS 2.0f
+#define OUT_OF_BOUNDS 8.0f
 
 void fitSpriteToWindow(sf::Sprite& sprite, sf::Window& window) //sets scale of the sprite to fit perfectly inside the screen
 {
@@ -27,7 +28,7 @@ complexNumber mandelbrotSetIteration(complexNumber z, complexNumber c)
 	return z * z + c;
 }
 
-int mandelbrotSet(complexNumber c) ///returns -1 if c is part of the mandelbrot set, or otherwise, the number of iterations the point took to get out of bounds
+unsigned short int mandelbrotSet(complexNumber c) ///returns 0 if c is part of the mandelbrot set, or otherwise, the number of iterations the point took to get out of bounds
 {
 
 	complexNumber z(0,0);
@@ -41,7 +42,7 @@ int mandelbrotSet(complexNumber c) ///returns -1 if c is part of the mandelbrot 
 		z = mandelbrotSetIteration(z,c);
 		it++;
 	}
-	return -1;
+	return 0;
 }
 
 
@@ -51,6 +52,7 @@ int main()
 	window.setFramerateLimit(60);
 	sf::Vector2f center;
 	float range, size;
+	
 	std::cout << "Select your center point:\n x:\n";
 	std::cin >> center.x;
 	std::cout << "y:\n";
@@ -59,9 +61,20 @@ int main()
 	std::cin >> range;
 	std::cout << "Select the canvas size to be rendered (pixels):\n";
 	std::cin >> size;
+	
+	//testing only
+	//center = sf::Vector2f(-0.75f, 0.0f);
+	//range = 2;
+	//size = 1000;
 
-	float scale = 1.0f / 125.0f;
 	sf::Vector2f canvasSize(size, size);
+
+	unsigned short int** canvas;
+	canvas = new unsigned short int* [canvasSize.x];
+	for (int i = 0; i < canvasSize.x; i++)
+	{
+		canvas[i] = new unsigned short int[canvasSize.y]{ 0 };
+	}
 
 	sf::RectangleShape background(canvasSize);
 	background.setFillColor(sf::Color(0, 0, 0, 255));
@@ -70,44 +83,52 @@ int main()
 	tex.create(canvasSize.x, canvasSize.y);
 	tex.draw(background);
 	tex.display();
+
 	sf::Sprite sprite(tex.getTexture());
 	fitSpriteToWindow(sprite, window);
 
 	sf::Vector2f origin(canvasSize/2.0f);
-	sf::RectangleShape pixel(sf::Vector2f(1.0f, 1.0f));
 
-
+	std::cout << "processing\n";
+	omp_set_num_threads(8);
 #pragma omp parallel for
-	for (int i = 0; i < canvasSize.x; i++)
+	for (int i = 0; i < (int)canvasSize.x; i++)
 	{
-		//std::cout << i << std::endl;
-		for (int j = 0; j < canvasSize.y; j++)
+		std::cout << i << std::endl;
+		for (int j = 0; j < (int)canvasSize.y; j++)
 		{
-			pixel.setPosition(i, j);
+			
 			
 			complexNumber c(center.x, center.y);
+
 			c.realPart += ((2 * i - canvasSize.x) / canvasSize.x)* range;
 			c.imaginaryPart += ((2 * j - canvasSize.y) / canvasSize.y) * range;
-
-
-			//complexNumber c(i-origin.x,j-origin.y);
-			//c *= complexNumber(scale, 0);
-			//std::cout << c;
-			int res = mandelbrotSet(c);
-			//std::cout << i <<' '<<j<<'\n';
-			
-
+			canvas[i][j] = mandelbrotSet(c);
+		}
+	}
+	std::cout << "coloring\n";
+	
+//#pragma omp parallel for
+	for (int i = 0; i < (int)canvasSize.x; i++)
+	{
+		std::cout << i << std::endl;
+		for (int j = 0; j < (int)canvasSize.y; j++)
+		{
 			sf::Vector3i color;
+			sf::RectangleShape pixel(sf::Vector2f(1.0f, 1.0f));
+			pixel.setPosition(i, j);
 			
-			if (res == -1)
+
+			if (canvas[i][j] == 0)
 				color = sf::Vector3i(0, 0, 0);
 			else
 				//color = HSVtoRGB(((res + 225) % 90) * 4, 80, 100);
-				color = HSVtoRGB(((res + 225) % 90) * 4 + (res + 225) % 4, 80, 100);
+				color = HSVtoRGB(((canvas[i][j] + 225) % 90) * 4 + (canvas[i][j] + 225) % 4, 80, 100);
 
-			pixel.setFillColor(sf::Color(color.x, color.y, color.z,255));
+			pixel.setFillColor(sf::Color(color.x, color.y, color.z, 255));
 			tex.draw(pixel);
 		}
+	
 	}
 
 	
