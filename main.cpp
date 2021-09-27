2e88347bfb9556265bca2b1bd33664c8f2316a79
Mihai Stefanoaia <mihaistefanoaia1102@gmail.com>
2021-09-27 14:25:01 +0300

    Improved the coloring and fixed some controls
    Managed to repair the coloring, making the program stop crashing and unreliable coloring by more properly implementing a lock. the WASD up and down controls were reversed and I fixed that

--- main.cpp
+++ main.cpp
@@ -52,6 +52,11 @@ int main()
 	window.setFramerateLimit(60);
 	sf::Vector2f center;
 	float range, size;
+
+	center = sf::Vector2f(-0.75f, 0.0f);
+	range = 2;
+	size = 250;
+
 	/*
 	std::cout << "Select your center point:\n x:\n";
 	std::cin >> center.x;
@@ -62,27 +67,16 @@ int main()
 	std::cout << "Select the canvas size to be rendered (pixels):\n";
 	std::cin >> size;
 	*/
-	//testing only
-	center = sf::Vector2f(-0.75f, 0.0f);
-	range = 2;
-	size = 250;
+	//^^^ leftover from old versions
+
 
 	sf::Vector2f canvasSize(size, size);
-	/*
-	unsigned short int** canvas;
-	canvas = new unsigned short int* [canvasSize.x];
-	for (int i = 0; i < canvasSize.x; i++)
-	{
-		canvas[i] = new unsigned short int[canvasSize.y]{ 0 };
-	}
-	*/
 	sf::RectangleShape background(canvasSize);
 	background.setFillColor(sf::Color(0, 0, 0, 255));
 
 	sf::RenderTexture tex;
 	tex.create(canvasSize.x, canvasSize.y);
 	tex.draw(background);
-	//tex.clear(sf::Color(0, 0, 0, 255));
 	tex.display();
 
 	sf::Sprite sprite(tex.getTexture());
@@ -139,13 +133,13 @@ int main()
 				needsUpdate = true;
 			}
 
-			if (evnt.text.unicode == 73 || evnt.text.unicode == 18)//down
+			if (evnt.text.unicode == 73 || evnt.text.unicode == 22)//down
 			{
 				center.y += (range / 10);
 				needsUpdate = true;
 			}
 
-			if (evnt.text.unicode == 74 || evnt.text.unicode == 22)//up
+			if (evnt.text.unicode == 74 || evnt.text.unicode == 18)//up
 			{
 				center.y -= (range / 10);
 				needsUpdate = true;
@@ -163,7 +157,6 @@ int main()
 				unsigned short int threadNum = omp_get_thread_num();
 				sf::RenderTexture tmpTexture;
 				tmpTexture.create(canvasSize.x, canvasSize.y);
-				//tmpTexture.clear(sf::Color(0,0,0,0));
 #pragma omp for
 				for (int i = 0; i < (int)canvasSize.x; i++)
 				{
@@ -187,35 +180,30 @@ int main()
 						if (res == 0)
 							color = sf::Vector3i(0, 0, 0);
 						else
-							//color = HSVtoRGB(((res + 225) % 90) * 4, 80, 100);
 							color = HSVtoRGB(((res + 225) % 90) * 4 + (res + 225) % 4, 80, 100);
 
 						pixel.setFillColor(sf::Color(color.x, color.y, color.z, 255));
-						//tex.draw(pixel);
 						tmpTexture.draw(pixel);
 					}
 
 				}
+				std::cout << "thread " << threadNum << "done processing\n";
 
-
-				std::cout<<"thread "<<threadNum<<"done\n";
-				while (order[threadNum] == false)	//wait until the threads before it draw their part onto the texture
-					continue;
-
-				sf::Sprite tmp(tmpTexture.getTexture());
-				tex.draw(tmp);
-
-				std::cout << "thread " << threadNum << "done drawing\n";
-				for (int i = 0; i < 1000; i++)	//give some time for synchronisation
-					continue;
-				/*
-				the app was prone to crashes and unreliable frame drawing (specifically stitching the texture chunks), caused by issues with simultaneous OpenGL calls (error 0xC0000005 in nvoglv64.dll)
-				the way that I fixed this was by spacing the drawing instructions by using an empty for loop (see the 2 lines above)
-				it worked surprisingly well
-				this is not a perfect fix, and the program still occasionally ends up with crashes and rendering artifacts, especially at the beginning, but it definitely helped a lot (went to right around the limit of 32bit floating point number limit for the zoom)
-				*/
+				while (true)
+				{
+					if (order[threadNum] == true)
+					{
+						//std::cout << "drawing: thread " << threadNum << "\n";
+						sf::Sprite tmp(tmpTexture.getTexture());
+						tex.draw(tmp);
+						order[threadNum + 1] = true;
+						//std::cout << order[threadNum + 1] << '\n';
+						std::cout << "done drawing: thread " << threadNum << "\n";
+						break;
+					}
+				}
 				
-				order[threadNum + 1] = true;
+				
 			}
 			needsUpdate = false;
 
